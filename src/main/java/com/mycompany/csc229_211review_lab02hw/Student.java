5d866898d6d74f0c063979ecbc6be11f52241486
OshcoshBagosh <oscargabrguerrero@gmail.com>
2024-02-07 11:13:15 -0500

    test

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,12 +4,31 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Person {
 	// ToDo 1: Make this class a child of Person
+    private double gpa;
 	
 	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
+
+    public String getAddress(){
+
+    }
+
+    @Override
+    public void setAddress(String address) {
+
+    }
+
+    // ToDo 3: Add a field for GPA and create setter and getter
+    public void setGPA(double gpa)
+    {
+        this.gpa = gpa;
+    }
+
+    public double getGPA()
+    {
+        return this.gpa;
+    }
 	
 	// ToDo 4: Add comments to your code
 
